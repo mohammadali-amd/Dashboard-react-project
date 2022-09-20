df3153e12093ea96db88e4018186828d90552483
mohammadali-amd <amidi1380@gmail.com>
2022-09-21 01:02:14 +0430

    updated

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
@@ -7,15 +7,35 @@ const initialState = {
    cart: false,
    userProfile: false,
    notification: false,
-}
+};
 
 export const ContextProvider = ({ children }) => {
-   const [activeMenu, setActiveMenu] = useState(true)
+   const [activeMenu, setActiveMenu] = useState(true);
+   const [screenSize, setScreenSize] = useState(undefined);
+   const [isClicked, setIsClicked] = useState(initialState);
+   const [currentColor, setCurrentColor] = useState('#03C9D7');
+   const [themeSettings, setThemeSettings] = useState(false);
+   const [currentMode, setCurrentMode] = useState('Light');
+
+   const setMode = (e) => {
+      setCurrentMode(e.target.value);
+      localStorage.setItem('colorMode', e.target.value);
+      setThemeSettings(false);
+   }
+
+   const setColor = (color) => {
+      setCurrentColor(color);
+      localStorage.setItem('colorMode', color);
+      setThemeSettings(false);
+   }
+
+   const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
    return (
-      <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+      <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
          {children}
       </StateContext.Provider>
    )
-}
+};
 
 export const useStateContext = () => useContext(StateContext);
