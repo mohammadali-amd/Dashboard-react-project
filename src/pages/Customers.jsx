a600f038475d36f3c1804460b8d3501280320bf6
mohammadali-amd <amidi1380@gmail.com>
2022-09-23 21:50:07 +0330

    Update Customers.jsx

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Sort, Selection, Toolbar, Filter, Page, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
+   const selectionsettings = { persistSelection: true }
+   const toolbarOptions = ['Delete']
+   const editing = { allowDeleting: true, allowEditting: true }
+
    return (
-      <div>Customers</div>
+      <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+         <Header category='Page' title='Customers' />
+         <GridComponent
+            dataSource={customersData}
+            enableHover={false}
+            allowSorting
+            allowPaging
+            editSettings={editing}
+            toolbar={toolbarOptions}
+            selectionSettings={selectionsettings}
+            pageSettings={{ pageCount: 5 }}
+         >
+            <ColumnsDirective>
+               {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+            </ColumnsDirective>
+            <Inject services={[Sort, Selection, Toolbar, Filter, Page, Edit]} />
+         </GridComponent>
+      </div>
    )
 }
 
